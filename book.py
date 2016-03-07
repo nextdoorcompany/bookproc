86c70b92e683885294d412f0c1dbfa6bb79bbf88
Justin Schechter <justin@nextdoorco.com>
2016-03-06 20:18:43 -0500

    some progress on the zip file

--- book.py
+++ book.py
@@ -1,3 +1,5 @@
+import os
+import zipfile
 import click
 
 @click.command()
@@ -6,7 +8,15 @@ def cli(indir):
     click.echo('job done')
 
 def make_zip(indir):
-    pass
+    #need to change directories here
+    zip_name = '1000.R0.F0.zip'
+    new_zip = zipfile.ZipFile(os.path.join(indir, zip_name), 'w')
+    for file in os.listdir(indir):
+        if file != zip_name:
+            full_file = os.path.join(indir, file)
+            new_zip.write(full_file, compress_type=zipfile.ZIP_DEFLATED)
+            os.remove(full_file)
+    new_zip.close()
 
 if __name__ == '__main__':
     cli()
--- test_book.py
+++ test_book.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import zipfile
 import pytest
 import book
 
@@ -11,11 +12,12 @@ def indir(request):
     request.addfinalizer(fin)
     return 'tmp'
 
-def test_book(indir):
+def test_zip(indir):
     open(os.path.join(indir, '1000.R0.F0 3HMF.pdf'), 'w')
 
     book.make_zip(indir)
 
     assert os.path.exists(os.path.join(indir, '1000.R0.F0.zip'))
+    assert '1000.R0.F0 3HMF.pdf' in zipfile.ZipFile(os.path.join(indir, '1000.R0.F0.zip')).namelist()
     assert not os.path.exists(os.path.join(indir, '1000.R0.F0 3HMF.pdf'))
     
