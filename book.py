af132abd1a6b9bb59f5662649b263359291130c0
Justin Schechter <justin@nextdoorco.com>
2016-03-08 21:39:25 -0500

    basic tests passing

--- book.py
+++ book.py
@@ -1,4 +1,5 @@
 import os
+import re
 import zipfile
 import click
 
@@ -8,15 +9,26 @@ def cli(indir):
     click.echo('job done')
 
 def make_zip(indir):
-    #need to change directories here
-    zip_name = '1000.R0.F0.zip'
-    new_zip = zipfile.ZipFile(os.path.join(indir, zip_name), 'w')
-    for file in os.listdir(indir):
-        if file != zip_name:
-            full_file = os.path.join(indir, file)
-            new_zip.write(full_file, compress_type=zipfile.ZIP_DEFLATED)
-            os.remove(full_file)
+    old_dir = os.getcwd()
+    os.chdir(indir)
+    job = ''
+    for file in os.listdir():
+        result = (re.match(r'\d*\.R\d+\.[FD]\d+', file))
+        if result:
+            job = result.group()
+            break
+
+    #what if there is no match?
+    file_list = [f for f in os.listdir() if job in f]
+
+    zip_name = job + '.zip'
+    new_zip = zipfile.ZipFile(zip_name, 'w')
+    for file in file_list:
+        new_zip.write(file, compress_type=zipfile.ZIP_DEFLATED)
+        os.remove(file)
+
     new_zip.close()
+    os.chdir(old_dir)
 
 if __name__ == '__main__':
     cli()
--- test_book.py
+++ test_book.py
@@ -12,7 +12,7 @@ def indir(request):
     request.addfinalizer(fin)
     return 'tmp'
 
-def test_zip(indir):
+def test_zip_frame(indir):
     open(os.path.join(indir, '1000.R0.F0 3HMF.pdf'), 'w')
 
     book.make_zip(indir)
@@ -20,4 +20,13 @@ def test_zip(indir):
     assert os.path.exists(os.path.join(indir, '1000.R0.F0.zip'))
     assert '1000.R0.F0 3HMF.pdf' in zipfile.ZipFile(os.path.join(indir, '1000.R0.F0.zip')).namelist()
     assert not os.path.exists(os.path.join(indir, '1000.R0.F0 3HMF.pdf'))
+
+def test_zip_door(indir):
+    open(os.path.join(indir, '1000.R0.D0 3HMD.pdf'), 'w')
+
+    book.make_zip(indir)
+
+    assert os.path.exists(os.path.join(indir, '1000.R0.D0.zip'))
+    assert '1000.R0.D0 3HMD.pdf' in zipfile.ZipFile(os.path.join(indir, '1000.R0.D0.zip')).namelist()
+    assert not os.path.exists(os.path.join(indir, '1000.R0.D0 3HMD.pdf'))
     
